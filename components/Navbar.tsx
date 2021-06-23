cac80196a4ffd5b6b84f7fb5a3b56faaaccf3f04
Christopher Trimboli <christopher.trimboli@protonmail.com>
2021-06-22 23:57:18 -0700

    navbar redux hookup

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import styled from "styled-components";
+import { useSelector, useDispatch } from 'react-redux';
+import { setName } from '../redux/slices/user';
 
 const NavbarContainer = styled.div`
      height: 60px;
@@ -8,9 +10,15 @@ const NavbarContainer = styled.div`
 `;
 
 export default function Navbar() {
+    const name = useSelector(state => state.user.name);
+    const dispatch = useDispatch();
+
+    console.log(name);
+
     return (
         <NavbarContainer>
-
+            <button onClick={() => dispatch(setName("hello"))}>hehe</button>
+            {name}
         </NavbarContainer>
     )
 }
