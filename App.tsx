9e02db7f7e9765bd19139e2afb88da0138528f59
Christopher Trimboli <christopher.trimboli@protonmail.com>
2021-06-22 22:57:13 -0700

    setup navbar

--- App.tsx
+++ App.tsx
@@ -4,11 +4,12 @@ import "@fontsource/rock-salt";
 import "@fontsource/yeseva-one";
 import "@fontsource/poppins";
 import "@fontsource/shadows-into-light";
+import Navbar from "./components/Navbar";
 
 function App() {
     return (
         <>
-            Hello
+            <Navbar />
         </>
     );
 };
---
+++ components/Navbar.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import styled from "styled-components";
+
+const NavbarContainer = styled.div`
+     height: 60px;
+     width: 100%;
+     background-color: pink;
+`;
+
+export default function Navbar() {
+    return (
+        <NavbarContainer>
+
+        </NavbarContainer>
+    )
+}
