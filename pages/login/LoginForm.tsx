452adb1834868472784badd101409d88cf581c85
Christopher Trimboli <christopher.trimboli@protonmail.com>
2021-06-24 23:33:55 -0700

    login form labels, submit button

--- pages/login/LoginForm.tsx
+++ pages/login/LoginForm.tsx
@@ -2,11 +2,19 @@ import React from "react";
 import styled from "styled-components";
 import { useAppSelector, useAppDispatch } from "../../redux/hooks";
 
-const LoginFormContainer = styled.div`
+const LoginFormContainer = styled.form`
     height: 100%;
 `;
 
+const EmailLabel = styled.label`
+
+`;
+
 const EmailInput = styled.input`
+
+`;
+
+const PasswordLabel = styled.label`
 
 `;
 
@@ -14,14 +22,25 @@ const PasswordInput = styled.input`
 
 `;
 
+const SubmitButton = styled.button`
+
+`;
+
 export default function LoginForm() {
     const name = useAppSelector(state => state.user.name);
     const dispatch = useAppDispatch();
 
+    const handleSubmit = () => {
+        console.log("Submit!");
+    };
+
     return (
-        <LoginFormContainer>
-            <EmailInput />
-            <PasswordInput />
+        <LoginFormContainer onSubmit={handleSubmit}>
+            <EmailLabel htmlFor="email">Email</EmailLabel>
+            <EmailInput type="email" id="email"/>
+            <PasswordLabel htmlFor="password">Password</PasswordLabel>
+            <PasswordInput type="password" id="password"/>
+            <SubmitButton>Submit</SubmitButton>
         </LoginFormContainer>
     )
 }
