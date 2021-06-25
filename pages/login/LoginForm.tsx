5beb41ee392afc520eeb8752aeed123c3da5f71c
Christopher Trimboli <christopher.trimboli@protonmail.com>
2021-06-25 00:25:25 -0700

    email / password setState loginForm

--- pages/login/LoginForm.tsx
+++ pages/login/LoginForm.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import { useAppSelector, useAppDispatch } from "../../redux/hooks";
 import { usePostLoginMutation } from "../../redux/api/login";
@@ -31,28 +31,27 @@ export default function LoginForm() {
     const name = useAppSelector(state => state.user.name);
     const dispatch = useAppDispatch();
 
+    const [email, setEmail] = useState<string>("");
+    const [password, setPassword] = useState<string>("");
+
     const [postLogin, { isLoading, isSuccess, isError, data }] = usePostLoginMutation();
 
     useEffect(() => {
         console.log(data, isSuccess, isError);
     }, [data, isSuccess, isError]);
 
-
     const handleSubmit = (e) => {
         e.preventDefault();
         console.log("Submit!");
-        postLogin({
-            email: "",
-            password: ""
-        })
+        postLogin({ email, password });
     };
 
     return (
         <LoginFormContainer onSubmit={handleSubmit}>
             <EmailLabel htmlFor="email">Email</EmailLabel>
-            <EmailInput type="email" id="email" />
+            <EmailInput type="email" id="email" onChange={(e) => setEmail(e.target.value)} />
             <PasswordLabel htmlFor="password">Password</PasswordLabel>
-            <PasswordInput type="password" id="password" />
+            <PasswordInput type="password" id="password" onChange={(e) => setPassword(e.target.value)} />
             <SubmitButton>Submit</SubmitButton>
         </LoginFormContainer>
     )
