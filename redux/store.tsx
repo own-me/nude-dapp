df43ce025a402abe45759e32e0be08d6da12c95c
Christopher Trimboli <christopher.trimboli@protonmail.com>
2021-06-24 23:59:46 -0700

    setup redux store with RTK query

--- redux/store.tsx
+++ redux/store.tsx
@@ -1,11 +1,19 @@
 import { configureStore } from "@reduxjs/toolkit"
+import { setupListeners } from "@reduxjs/toolkit/query"
+import { loginApi } from "./api/login"
 import userReducer from "./slices/user";
 
 export const store = configureStore({
     reducer: {
-        user: userReducer
-    }
+        user: userReducer,
+        [loginApi.reducerPath]: loginApi.reducer,
+    },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(
+        loginApi.middleware
+    ),
 });
+
+setupListeners(store.dispatch);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
