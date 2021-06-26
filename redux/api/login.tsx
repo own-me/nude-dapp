fd9e9188ece3ce37db2dcab35408ea8aad808038
Christopher Trimboli <christopher.trimboli@protonmail.com>
2021-06-25 23:38:14 -0700

    add more login api types

--- redux/api/login.tsx
+++ redux/api/login.tsx
@@ -1,16 +1,21 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-interface Login {
+interface LoginRequest {
     email?: string,
     password?: string,
     token?: string
+}
+
+interface LoginResponse {
+    token: string,
+    message: string
 }
 
 export const loginApi = createApi({
     reducerPath: "loginApi",
     baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3000/" }),
     endpoints: (builder) => ({
-        postLogin: builder.mutation<Login, any>({
+        postLogin: builder.mutation<LoginResponse, LoginRequest>({
             query: (body) => ({
                 url: "login/",
                 method: "POST",
