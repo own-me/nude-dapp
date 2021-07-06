1a44053e970d10c57f35a26be2d48816048215aa
Christopher Trimboli <christopher.trimboli@protonmail.com>
2021-07-05 22:49:13 -0700

    copy styles over to register page

--- pages/register/RegisterForm.tsx
+++ pages/register/RegisterForm.tsx
@@ -2,40 +2,92 @@ import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import { useHistory } from "react-router-dom";
 import { usePostRegisterMutation } from "../../redux/api/register";
+import loadingSpinner from "../../media/loading.svg";
 
 const RegisterFormContainer = styled.form`
-    height: 200px;
     display: flex;
     flex-direction: column;
     align-items: center;
+    background-color: #FFEAFF;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    border-radius: 25px;
+    border: 1px solid #FEB8FF;
+    padding: 50px 100px;
+    color: black;
+`;
+
+const RegisterHeader = styled.h1`
+    font-family: Rock Salt, Open Sans;
+    color: #c931ff;
 `;
 
 const EmailLabel = styled.label`
-
+    color: black;
+    font-family: Poppins, Open Sans;
+    font-size: 22px;
 `;
 
 const EmailInput = styled.input`
-
+    background: #FFFFFF;
+    border: 1px solid #FFA2FB;
+    box-sizing: border-box;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    border-radius: 5px;
+    padding: 8px 15px;
+    margin-bottom: 20px;
+    font-size: 18px;
 `;
 
 const PasswordLabel = styled.label`
-
+    color: black;
+    font-family: Poppins, Open Sans;
+    font-size: 22px;
 `;
 
 const PasswordInput = styled.input`
-
+    background: #FFFFFF;
+    border: 1px solid #FFA2FB;
+    box-sizing: border-box;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    border-radius: 5px;
+    margin-bottom: 20px;
+    padding: 8px 15px;
+    font-size: 18px;
 `;
 
 const ConfirmPasswordLabel = styled(PasswordLabel)`
-
+    color: black;
+    font-family: Poppins, Open Sans;
+    font-size: 22px;
 `;
 
 const ConfirmPasswordInput = styled(PasswordInput)`
-
+    background: #FFFFFF;
+    border: 1px solid #FFA2FB;
+    box-sizing: border-box;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    border-radius: 5px;
+    margin-bottom: 20px;
+    padding: 8px 15px;
+    font-size: 18px;
 `;
 
 const SubmitButton = styled.button`
-    margin: 10px;
+    margin: 20px;
+    background: #f455fa;
+    border: 1px solid #707070;
+    box-sizing: border-box;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    border-radius: 5px;
+    color: white;
+    padding: 8px 25px;
+    font-family: Poppins, Open Sans;
+    font-size: 20px;
+    cursor: pointer;
+
+    :hover{
+        background: #ff44e6;  
+    }
 `;
 
 export default function RegisterForm() {
@@ -66,13 +118,16 @@ export default function RegisterForm() {
 
     return (
         <RegisterFormContainer onSubmit={handleSubmit}>
+            <RegisterHeader>Register</RegisterHeader>
             <EmailLabel htmlFor="email">Email</EmailLabel>
             <EmailInput type="email" id="email" onChange={(e) => setEmail(e.target.value)} required autoComplete="email" />
             <PasswordLabel htmlFor="password">Password</PasswordLabel>
             <PasswordInput type="password" id="password" onChange={(e) => setPassword(e.target.value)} required />
             <ConfirmPasswordLabel htmlFor="confirmPassword">Confirm Password</ConfirmPasswordLabel>
             <ConfirmPasswordInput type="password" id="confirmPassword" onChange={(e) => setConfirmPassword(e.target.value)} required />
-            <SubmitButton disabled={!isPasswordConfirmed}>Submit</SubmitButton>
+            {
+                isLoading ? <img src={loadingSpinner} /> : <SubmitButton disabled={!isPasswordConfirmed}>Submit</SubmitButton>
+            }
         </RegisterFormContainer>
     );
 };
--- pages/register/RegisterPage.tsx
+++ pages/register/RegisterPage.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import styled from "styled-components";
 import RegisterForm from "./RegisterForm";
+import loginBackground from "../../media/login-background.svg";
+import logo from "../../media/navbar.png";
 
 const RegisterPageContainer = styled.div`
     height: 100%;
@@ -8,12 +10,22 @@ const RegisterPageContainer = styled.div`
     justify-content: center;
     align-items: center;
     flex-direction: column;
+    background-image: url(${loginBackground});
+    background-size: cover;
+    background-color: white;
+`;
+
+const LogoImg = styled.img`
+    position: absolute;
+    top: 50px;
+    left: 50px;
+    height: 100px;
 `;
 
 export default function RegisterPage() {
     return (
         <RegisterPageContainer>
-            <h1>Register</h1>
+            <LogoImg src={logo} />
             <RegisterForm />
         </RegisterPageContainer>
     )
