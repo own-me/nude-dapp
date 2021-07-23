522fda667e2aad213ee618545966902cf135410e
Christopher Trimboli <christopher.trimboli@protonmail.com>
2021-07-22 23:20:11 -0700

    fetch ABI helpers

--- lib/helpers.ts
+++ lib/helpers.ts
@@ -7,3 +7,13 @@ export function shortenAddress(address: string) {
 export function formatEth(value: BigNumber) {
     return value && ethers.FixedNumber.fromValue(value, 18).round(4).toString();
 };
+
+export async function fetchNudeABI() {
+    const res = await fetch(`https://raw.githubusercontent.com/the-digital-labs/own-me-contracts/master/build/Nude.json`);
+    return res.json();
+}
+
+export async function fetchNudeNftABI() {
+    const res = await fetch(`https://raw.githubusercontent.com/the-digital-labs/own-me-contracts/master/build/NudeNFT.json`);
+    return res.json();
+}
