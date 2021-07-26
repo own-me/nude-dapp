ef6d584214e75193a06014af205c3477049a811c
Christopher Trimboli <christopher.trimboli@protonmail.com>
2021-07-25 23:07:09 -0700

    new verifyEmail API handler

--- redux/api/register.tsx
+++ redux/api/register.tsx
@@ -3,10 +3,19 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 interface RegisterRequest {
     email: string,
     name: string,
-    password: string
+    password: string,
+    verificationCode: string
 }
 
 interface RegisterResponse {
+    message: string,
+}
+
+interface VerifyEmailRequest {
+    email: string
+}
+
+interface VerifyEmailResponse {
     message: string,
 }
 
@@ -15,13 +24,27 @@ export const registerApi = createApi({
     baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3000/" }),
     endpoints: (builder) => ({
         postRegister: builder.mutation<RegisterResponse, RegisterRequest>({
-            query: (body) => ({
+            query: ({ email, name, password, verificationCode }) => ({
                 url: "register/",
                 method: "POST",
-                body
+                body: {
+                    email,
+                    name,
+                    password,
+                    verificationCode
+                }
+            }),
+        }),
+        verifyEmail: builder.mutation<VerifyEmailResponse, VerifyEmailRequest>({
+            query: ({ email }) => ({
+                url: "register/verify-email",
+                method: "POST",
+                body: {
+                    email
+                }
             }),
         }),
     }),
 });
 
-export const { usePostRegisterMutation } = registerApi;
+export const { usePostRegisterMutation, useVerifyEmailMutation } = registerApi;
