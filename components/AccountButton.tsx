8cc14a3e1ba6d04636f8e2321ceeb6faa8ed9140
Christopher Trimboli <christopher.trimboli@protonmail.com>
2021-08-01 16:57:07 -0700

    restyle account dropdown

--- components/AccountButton.tsx
+++ components/AccountButton.tsx
@@ -1,14 +1,14 @@
 import React, { useState, useMemo } from "react";
 import styled from "styled-components";
 import Dropdown from "./Dropdown";
-import UserBadge from "./UserBadge";
-import WalletBalance from "./WalletBalance";
-import LogoutButton from "./LogoutButton";
-import SendTxButton from "./SendTxButton";
 import useWallet from "../hooks/useWallet";
-import { formatEth } from "../lib/helpers";
+import { formatEth, shortenAddress } from "../lib/helpers";
+import { useAppSelector } from "../redux/hooks";
+import { useAppDispatch } from "../redux/hooks";
+import { setUserLoggedIn } from "../redux/slices/user";
+import { Link } from "react-router-dom";
 
-const AccountButtonContainer = styled.button`
+const AccountButtonTemplate = styled.button`
     font-family: Poppins, Open Sans;
     font-size: 22px;
     background-color: #FF81EB;
@@ -18,26 +18,83 @@ const AccountButtonContainer = styled.button`
     border-radius: 6px;
     margin-left: 50px;
     box-shadow: 0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23);
+    cursor: pointer;
+
+    :hover {
+        background-color: #fb5de1;
+    }
+`;
+
+const AccountName = styled.div`
+    font-family: Poppins, Open Sans;
+    color: #282828;
+    font-size: 24px;
+`;
+
+const AccountInfo = styled.div`
+    font-family: Poppins, Open Sans;
+    color: #282828;
+    font-size: 18px;
+    margin: 10px 0;
+`;
+
+const Divider = styled.hr`
+    border-color: #FFB0F7;
+`;
+
+const EditProfileButton = styled(AccountButtonTemplate)`
+    width: 100%;
+    margin-left: 0;
+    padding: 6px 10px;
+    font-size: 16px;
+    margin: 10px 0;
+`;
+
+const LogoutText = styled.div`
+    font-family: Poppins, Open Sans;
+    color: purple;
+    text-align: center;
+    text-decoration: underline;
+    cursor: pointer;
+
+    :hover {
+        color: #FFB0F7;
+    }
 `;
 
 export default function AccountButton() {
+    const dispatch = useAppDispatch();
     const [isOpen, setIsOpen] = useState<boolean>(false);
     const { balance, address } = useWallet();
+    const email = useAppSelector(state => state.user.email);
+    const name = useAppSelector(state => state.user.name);
 
     const formattedBalance = useMemo(() => formatEth(balance), [balance]);
+    const formattedAddress = useMemo(() => shortenAddress(address, 16), [address]);
+
+    const handleLogout = () => {
+        dispatch(setUserLoggedIn(false));
+    };
 
     return (
         <>
-            <AccountButtonContainer type="button" onClick={() => setIsOpen(!isOpen)}>
+            <AccountButtonTemplate type="button" onClick={() => setIsOpen(!isOpen)}>
                 {formattedBalance} NUDE
-            </AccountButtonContainer>
+            </AccountButtonTemplate>
             {
                 isOpen &&
                 <Dropdown>
-                    <UserBadge />
-                    <WalletBalance />
-                    <LogoutButton />
-                    <SendTxButton />
+                    <div>
+                        <AccountName>{name}</AccountName>
+                        <Divider />
+                        <AccountInfo>{formattedAddress}</AccountInfo>
+                        <AccountInfo>{email}</AccountInfo>
+                        <Link to={`/${name}`}><EditProfileButton>Edit Profile</EditProfileButton></Link>
+                    </div>
+                    <br />
+                    <LogoutText onClick={handleLogout}>
+                        Logout
+                    </LogoutText>
                 </Dropdown>
             }
         </>
--- components/Dropdown.tsx
+++ components/Dropdown.tsx
@@ -2,15 +2,17 @@ import React from "react";
 import styled from "styled-components";
 
 const DropdownContainer = styled.div`
-    padding: 50px 30px;
-    background: white;
+    padding: 40px 40px 20px 40px;
+    background: #FDE5FE;
     color: black;
     position: fixed;
-    top: 90px;
-    right: 0;
+    top: 95px;
+    right: 5px;
     align-content: center;
     align-items: center;
     flex-direction: column;
+    border-radius: 10px;
+    border: 1px solid #FECDFF;
 `;
 
 export default function Dropdown({ children }) {
