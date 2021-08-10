9a3026b7cfdd6738f24ce38c76212ab847a9cba0
Christopher Trimboli <christopher.trimboli@protonmail.com>
2021-08-09 23:02:14 -0700

    add editProfileMutation api

--- redux/api/user.tsx
+++ redux/api/user.tsx
@@ -26,6 +26,17 @@ interface UploadProfileImageResponse {
     url: string
 }
 
+interface EditProfileResponse {
+    message: string,
+}
+interface EditProfileRequest {
+    oldName: string,
+    newName: string,
+    bio: string,
+    profileImageUrl: string,
+    bannerImageUrl: string
+}
+
 export const userApi = createApi({
     reducerPath: "userApi",
     baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3000/" }),
@@ -47,7 +58,19 @@ export const userApi = createApi({
                 }
             }),
         }),
+        editProfile: builder.mutation<EditProfileResponse, EditProfileRequest>({
+            query: ({ oldName, newName, bio, profileImageUrl, bannerImageUrl }) => ({
+                url: `user/edit/${oldName}`,
+                method: "POST",
+                body: {
+                    newName,
+                    bio,
+                    profileImageUrl,
+                    bannerImageUrl
+                }
+            }),
+        }),
     }),
 });
 
-export const { useGetUserQuery, useUploadProfileImageMutation } = userApi;
+export const { useGetUserQuery, useUploadProfileImageMutation, useEditProfileMutation } = userApi;
