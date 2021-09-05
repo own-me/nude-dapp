f895b570e510dd982b49f09c4807fa4861b58b78
Christopher Trimboli <christopher.trimboli@protonmail.com>
2021-09-04 22:35:36 -0700

    add breakpoints / remove lib file

--- lib/device-breakpoints.ts
+++
@@ -1,5 +0,0 @@
-export const DeviceBreakpoints: Record<string, number> = {
-    MOBILE: 768,
-    TABLET: 992,
-    DESKTOP: 1200
-}
---
+++ styles/theme.ts
@@ -0,0 +1,9 @@
+import {DefaultTheme} from "styled-components";
+
+export const theme: DefaultTheme = {
+    breakpoints: {
+        mobile: 768,
+        tablet: 992,
+        desktop: 1200
+    }
+};
