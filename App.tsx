de9248e81986f856ff5c8487c672dabfcd5903c9
Christopher Trimboli <christopher.trimboli@protonmail.com>
2021-09-04 22:34:45 -0700

    add styled components theme file / provider

--- App.tsx
+++ App.tsx
@@ -8,13 +8,16 @@ import { store } from './redux/store';
 import { Provider } from 'react-redux'
 import Main from "./Main";
 import { BrowserRouter as Router } from "react-router-dom";
-
+import { ThemeProvider } from "styled-components";
+import { theme } from "./styles/theme";
 
 function App() {
     return (
         <Provider store={store}>
             <Router>
-                <Main />
+                <ThemeProvider theme={theme}>
+                    <Main />
+                </ThemeProvider>
             </Router>
         </Provider>
     );
---
+++ lib/device-breakpoints.ts
@@ -0,0 +1,5 @@
+export const DeviceBreakpoints: Record<string, number> = {
+    MOBILE: 768,
+    TABLET: 992,
+    DESKTOP: 1200
+}
