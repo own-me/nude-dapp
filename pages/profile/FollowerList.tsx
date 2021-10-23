b0fce76d5b61463e9c2ace4447eb25cc19c7d432
Christopher Trimboli <christopher.trimboli@protonmail.com>
2021-10-22 23:33:27 -0700

    follow list row image, address, styling

--- pages/profile/FollowerList.tsx
+++ pages/profile/FollowerList.tsx
@@ -1,6 +1,8 @@
 import React, { memo } from "react";
 import styled from "styled-components";
 import { Follower } from "../../redux/api/follow";
+import defaultProfile from "../../media/defaults/missing-profile.png";
+import { shortenAddress } from "../../lib/helpers";
 
 interface FollowListProps {
     followers?: Follower[];
@@ -11,19 +13,27 @@ const FollowerListContainer = styled.div`
 `;
 
 const FollowerListRow = styled.div`
-
+    width: 100%;
+    display: flex;
+    padding: 20px 40px;
 `;
 
 const FollowerProfileImage = styled.img`
-
+    width: 125px;
+    height: 125px;
+    border-radius: 100%;
+    border: 5px solid white;
+    background: white;
+    box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
 `;
 
 const FollowerInfoContainer = styled.div`
-
+    display: flex;
+    padding: 20px 40px;
 `;
 
 const FollowerInfoAddress = styled.div`
-
+    font-family: Poppins, Open Sans;
 `;
 
 const FollowerInfoName = styled.div`
@@ -40,9 +50,9 @@ const FollowerList = memo(({ followers = [] }: FollowListProps) => {
             {
                 followers.map((follower: Follower, index) =>
                     <FollowerListRow key={index}>
-                        <FollowerProfileImage src="" />
+                        <FollowerProfileImage src={defaultProfile} />
                         <FollowerInfoContainer>
-                            <FollowerInfoAddress>{follower.fromAddress}</FollowerInfoAddress>
+                            <FollowerInfoAddress>{shortenAddress(follower.fromAddress, 16)}</FollowerInfoAddress>
                         </FollowerInfoContainer>
                         <FollowerStatsContainer>
 
