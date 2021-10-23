b2db4597860fbb69272b8f0afa6554f449a032c3
Christopher Trimboli <christopher.trimboli@protonmail.com>
2021-10-23 13:02:32 -0700

    followerlist follow button

--- pages/profile/FollowerList.tsx
+++ pages/profile/FollowerList.tsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import { Follower } from "../../redux/api/follow";
 import defaultProfile from "../../media/defaults/missing-profile.png";
 import { shortenAddress } from "../../lib/helpers";
+import { usePostFollowMutation } from "../../redux/api/follow";
 
 interface FollowListProps {
     followers?: Follower[];
@@ -34,10 +35,29 @@ const FollowerInfoContainer = styled.div`
 
 const FollowerInfoAddress = styled.div`
     font-family: Poppins, Open Sans;
+    font-size: 18px;
 `;
 
 const FollowerInfoName = styled.div`
 
+`;
+
+const FollowButton = styled.button`
+    font-family: Poppins, Open Sans;
+    font-size: 14px;
+    background-color: #71A1FF;
+    color: white;
+    border: none;
+    padding: 0px 15px;
+    border-radius: 25px;
+    box-shadow: 0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23);
+    height: 25px;
+    margin-left: 20px;
+    cursor: pointer;
+
+    :hover {
+        background-color: #3e7fff;
+    }
 `;
 
 const FollowerStatsContainer = styled.div`
@@ -45,6 +65,19 @@ const FollowerStatsContainer = styled.div`
 `;
 
 const FollowerList = memo(({ followers = [] }: FollowListProps) => {
+
+    const [postFollow, {
+        isLoading: isPostFollowLoading,
+        isSuccess: isPostFollowSuccess,
+        isError: isPostFollowError,
+        data: postFollowData,
+        error: postFollowError
+    }] = usePostFollowMutation();
+
+    const handleFollow = (toAddress: string) => {
+        postFollow({ toAddress })
+    };
+
     return (
         <FollowerListContainer>
             {
@@ -53,6 +86,7 @@ const FollowerList = memo(({ followers = [] }: FollowListProps) => {
                         <FollowerProfileImage src={defaultProfile} />
                         <FollowerInfoContainer>
                             <FollowerInfoAddress>{shortenAddress(follower.fromAddress, 16)}</FollowerInfoAddress>
+                            <FollowButton onClick={() => handleFollow(follower.fromAddress)}>Follow</FollowButton>
                         </FollowerInfoContainer>
                         <FollowerStatsContainer>
 
