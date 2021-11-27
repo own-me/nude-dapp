cdb956a533c4e32514cde0cf10f84f1430293548
Christopher Trimboli <christopher.trimboli@protonmail.com>
2021-11-27 09:37:08 -0800

    add postsPost API more posts interfaces

--- redux/api/posts.tsx
+++ redux/api/posts.tsx
@@ -1,20 +1,32 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 interface GetPostRequest {
-	postId: string;
+    postId: string;
 }
 
 interface GetPostResponse {
-	id: string;
-	childOf?: string;
-	text: string;
-	creatorAddress: string;
-	creatorName?: string;
+    id: string;
+    childOf?: string;
+    text: string;
+    creatorAddress: string;
+    creatorName?: string;
     dateCreated: string;
     likesCount: number;
     commentCount: number;
     imageUrl?: string;
     profileImageUrl?: string;
+}
+
+interface PostsPostRequest {
+    childOf?: string;
+    text: string;
+    creatorAddress: string;
+    imageUrl?: string;
+}
+
+interface PostsPostResponse {
+    message?: string;
+    error?: string;
 }
 
 export const postsApi = createApi({
@@ -29,8 +41,23 @@ export const postsApi = createApi({
                     Authorization: `Bearer ${localStorage.getItem("token")}`
                 }
             })
+        }),
+        postsPost: builder.mutation<PostsPostResponse, PostsPostRequest>({
+            query: ({ childOf, text, creatorAddress, imageUrl }) => ({
+                url: "posts/",
+                method: "POST",
+                body: {
+                    childOf,
+                    text,
+                    creatorAddress,
+                    imageUrl
+                },
+                headers: {
+                    Authorization: `Bearer ${localStorage.getItem("token")}`
+                }
+            })
         })
     })
 });
 
-export const { useGetPostQuery } = postsApi;
+export const { useGetPostQuery, usePostsPostMutation } = postsApi;
