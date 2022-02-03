afc2edb05ce65f1a6aa7303b4a6096c0a30d3134
Christopher Trimboli <christopher.trimboli@protonmail.com>
2022-02-02 23:02:25 -0800

    report modal header

--- components/ReportModal.tsx
+++ components/ReportModal.tsx
@@ -1,7 +1,15 @@
 import React, { memo } from "react";
+import styled from "styled-components";
 import { useAppDispatch, useAppSelector } from "../redux/hooks";
 import { toggleReportModal } from "../redux/slices/app";
 import Modal from "./Modal";
+
+const Header = styled.h1`
+    font-family: "Poppins", sans-serif;
+    padding: 0px 40px 20px 40px;
+    border-bottom: 1px solid #e0e0e0;
+    margin-bottom: 0px;
+`;
 
 const ReportModal = memo(() => {
     const dispatch = useAppDispatch();
@@ -9,7 +17,7 @@ const ReportModal = memo(() => {
 
     return (
         <Modal isOpen={isReportModalOpen} onClose={() => dispatch(toggleReportModal())}>
-            <p>Report</p>
+            <Header>Report</Header>
         </Modal>
     );
 });
