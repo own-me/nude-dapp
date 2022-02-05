40d1da6a1cb930152a585fdc11eacf0e3e1ebf7a
Christopher Trimboli <christopher.trimboli@protonmail.com>
2022-02-05 14:59:43 -0800

    form checkbox input component

---
+++ components/FormCheckboxInput.tsx
@@ -0,0 +1,46 @@
+import React, { memo, useCallback, useState } from "react";
+import styled from "styled-components";
+import { useAppSelector } from "../redux/hooks";
+
+export const FormFileInputContainer = styled.div<{ $isDarkMode: boolean }>`
+    width: 100%;
+    cursor: pointer;
+    font-family: Poppins, Open Sans;
+    color: ${props => props.$isDarkMode ? props.theme.dark.textColor : props.theme.light.textColor};
+`;
+
+const CheckboxInput = styled.input`
+    height: 25px;
+    width: 25px;
+    margin-right: 10px;
+    cursor: pointer;
+`;
+
+const Label = styled.label`
+
+`;
+
+interface FormCheckboxInputProps {
+    label: string;
+    onChecked?: (checked: boolean) => void;
+}
+
+const FormCheckboxInput = memo(({ label, onChecked }: FormCheckboxInputProps) => {
+    const [checked, setChecked] = useState<boolean>(false);
+
+    const isDarkMode = useAppSelector(state => state.app.isDarkMode);
+
+    const handleChange = useCallback(() => {
+        onChecked && onChecked(!checked);
+        setChecked(!checked);
+    }, [checked, onChecked]);
+
+    return (
+        <FormFileInputContainer $isDarkMode={isDarkMode}>
+            <CheckboxInput onChange={handleChange} type="checkbox" value={String(checked)}/>
+            <Label>{label}</Label>
+        </FormFileInputContainer>
+    );
+});
+
+export default FormCheckboxInput;
