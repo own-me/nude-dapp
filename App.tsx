710565f45a667669c3fc626d8d85ec3bfdeb4b95
Christopher Trimboli <christopher.trimboli@protonmail.com>
2022-04-11 23:22:56 -0700

    use HashRouter instead

--- App.tsx
+++ App.tsx
@@ -7,18 +7,18 @@ import "@fontsource/shadows-into-light";
 import { store } from "./redux/store";
 import { Provider } from "react-redux";
 import Main from "./Main";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
 import { theme } from "./styles/theme";
 
 function App() {
     return (
         <Provider store={store}>
-            <Router>
+            <HashRouter>
                 <ThemeProvider theme={theme}>
                     <Main />
                 </ThemeProvider>
-            </Router>
+            </HashRouter>
         </Provider>
     );
 }
