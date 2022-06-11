f1b3fabba17e506041d5819038b595bfffdcc172
Sylvia Makuch <sylvia.makuch@protonmail.com>
2022-06-11 14:14:43 -0600

    Toggle button working b/w buy/sell with directional change of layout in form to accomidate the change b/w

--- pages/buytokens/BuyTokensPage.tsx
+++ pages/buytokens/BuyTokensPage.tsx
@@ -1,6 +1,12 @@
 import styled from "styled-components";
 import pinkCandy from "../../media/pink-candy.svg";
-import { MintPageContainer, MintFormHeader, MintFormHeaderTitle, MintFormHeaderCandy, SubmitButton } from "../mint/MintPage";
+import {
+    MintPageContainer,
+    MintFormHeader,
+    MintFormHeaderTitle,
+    MintFormHeaderCandy,
+    SubmitButton,
+} from "../mint/MintPage";
 import React, { useState } from "react";
 import { useAppSelector } from "../../redux/hooks";
 import { Nude__factory } from "../../typechain/factories/Nude__factory";
@@ -9,15 +15,15 @@ import { ethers } from "ethers";
 import arrow from "../../media/arrow.svg";
 import candylogo from "../../media/candylogo.svg";
 
-const MintTitle = styled(MintFormHeaderTitle)`
+const NudeTitle = styled(MintFormHeaderTitle)`
   color: #dc68f9;
 `;
 
-const BuyTokensPageContainer = styled(MintPageContainer) <{ $isDarkMode: boolean }>`
+const NudeSwapContainer = styled(MintPageContainer) <{ $isDarkMode: boolean }>`
   display: flex;
   flex-direction: column;
   align-items: center;
-  background-color: ${props => props.$isDarkMode ? "#1b0028" : "#fef4fb"};
+  background-color: ${(props) => (props.$isDarkMode ? "#1b0028" : "#fef4fb")};
   border: 6px dotted #dc68f9;
   padding: 50px;
   height: 80%;
@@ -34,113 +40,137 @@ const PageContainer = styled.div`
 `;
 
 const InputContainer = styled.div<{ $isDarkMode: boolean }>`
-    display: flex;
-    flex-direction: row;
-    justify-content: space-around;
-    align-items: center;
-    align-content: center;
-    width: 100%;
-    height: 80px;
-    border-radius: 5px;
-    background-color: ${props => props.$isDarkMode ? "#1b0028" : "#ffffff"};
-    color: ${props => props.$isDarkMode ? "#ffffff" : "#000000"};
-    font-size: 20px;
-    font-family: Poppins, Open Sans;
-    font-weight: bold;
-    border: 1px solid #cc00ff;
-    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+  align-content: center;
+  width: 100%;
+  height: 80px;
+  border-radius: 5px;
+  background-color: ${(props) => (props.$isDarkMode ? "#1b0028" : "#ffffff")};
+  color: ${(props) => (props.$isDarkMode ? "#ffffff" : "#000000")};
+  font-size: 20px;
+  font-family: Poppins, Open Sans;
+  font-weight: bold;
+  border: 1px solid #cc00ff;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
 `;
 
 const ToggleContainer = styled.div<{ $isDarkMode: boolean }>`
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-    align-items: center;
-    margin: 10px;
-    width: 100%;
-    height: 80px;
-    border-radius: 5px;
-    background-color: ${props => props.$isDarkMode ? "#1b0028" : "#ffffff"};
-    color: ${props => props.$isDarkMode ? "#ffffff" : "#000000"};
-    font-size: 20px;
-    font-family: Poppins, Open Sans;
-    font-weight: bold;
-    border: 1px solid #cc00ff;
-    margin: 30px 0px;
-    padding: 8px;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  margin: 10px;
+  width: 100%;
+  height: 80px;
+  border-radius: 5px;
+  background-color: ${(props) => (props.$isDarkMode ? "#1b0028" : "#ffffff")};
+  color: ${(props) => (props.$isDarkMode ? "#ffffff" : "#000000")};
+  font-size: 20px;
+  font-family: Poppins, Open Sans;
+  font-weight: bold;
+  border: 1px solid #cc00ff;
+  margin: 30px 0px;
+  padding: 8px;
 `;
 
 const Label = styled.h3`
-    text-align: left;
-    display: block;
-    font-size: 26px;
+  text-align: left;
+  display: block;
+  font-size: 26px;
 `;
 
-const InputDiv = styled.div`
-    width: 100%;
-    display: flex;
-    flex-direction: column;
+const InputDiv = styled.div  <{ $isSellActive: boolean }>`
+  width: 100%;
+  display: flex;
+  flex-direction: ${(props) => (props.$isSellActive ? "column-reverse" : "column")};
 `;
 
 const TransferButton = styled(SubmitButton)`
-    width: 50%;
-    padding: 20px;
-    margin-top: 35px;
+  width: 50%;
+  padding: 20px;
+  margin-top: 35px;
 `;
 
 const Arrow = styled.img`
-    width: 40px;
-    height: 40px;
-    margin-top: 10px;
-    position: relative;
-    left: 48%;
-    top: 40px;
+  width: 40px;
+  height: 40px;
+  margin-top: 10px;
+  position: relative;
+  left: 48%;
+  top: 40px;
 `;
 
-const BuyToken = styled(SubmitButton)`
-    width: 48%;
-    padding: 20px;
-    margin: 0;
-    border-radius: 6px 0px 0px 6px;
-    box-shadow: none;
+const BuyTokenButton = styled(SubmitButton) <{ $isBuyActive: boolean } >`
+  width: 48%;
+  padding: 20px;
+  margin: 0;
+  border-radius: 6px 0px 0px 6px;
+  box-shadow: none;
+  background-color: #cc00ff;
+  background-color: ${(props) => (props.$isBuyActive ? "#cc00ff" : "#EDC7E7")}; ;
 `;
 
-const SellToken = styled(SubmitButton)`
-    width: 48%;
-    padding: 20px;
-    margin: 0;
-    border-radius: 0px 6px 6px 0px;
-    box-shadow: none;
-    background-color: #EDC7E7;
+const SellTokenButton = styled(SubmitButton) <{ $isSellActive: boolean }>`
+  width: 48%;
+  padding: 20px;
+  margin: 0;
+  border-radius: 0px 6px 6px 0px;
+  box-shadow: none;
+  background-color: ${(props) => (props.$isSellActive ? "#cc00ff" : "#EDC7E7")};
 `;
+
 const TokenDropdown = styled.select`
-    font-size: 24px;
-    font-family: Poppins, Open Sans;
-    font-weight: bold;
+  font-size: 24px;
+  font-family: Poppins, Open Sans;
+  font-weight: bold;
 `;
 
-const TokenAmoutnInput = styled.input`
-    font-size: 24px;
-    font-family: Poppins, Open Sans;
-    font-weight: bold;
-    border: none;
-    `;
+const TokenAmoutnInput = styled.input<{ $isDarkMode: boolean }>`
+  font-size: 24px;
+  font-family: Poppins, Open Sans;
+  font-weight: bold;
+  border: none;
+  background-color: ${(props) => (props.$isDarkMode ? "#1b0028" : "#ffffff")};
+  color: ${(props) => (props.$isDarkMode ? "#ffffff" : "#000000")};
+`;
 
 const MaxButton = styled(SubmitButton)`
-    width: 22%;
-    margin: 0px;
+  width: 22%;
+  margin: 0px;
 `;
 
 const CandyLogo = styled.img`
-    width: 40px;
-    height: 40px;
+  width: 40px;
+  height: 40px;
+  position: relative;
+  top: 6px;
+  right: 22px;
+`;
+
+const InputDivider = styled.div`
+  width: 22%;
 `;
 
 export default function BuyTokensPage() {
     const [price, setPrice] = useState("0");
+    const [isBuyActive, setBuyIsActive] = useState(false);
+    const [isSellActive, setSellIsActive] = useState(false);
 
-    const isDarkMode = useAppSelector(state => state.app.isDarkMode);
+    const handleBuyClick = () => {
+        setBuyIsActive(!isBuyActive);
+        setSellIsActive(false);
+    };
 
+    const handleSellClick = () => {
+        setSellIsActive(!isSellActive);
+        setBuyIsActive(false);
+    };
+
+
+    const isDarkMode = useAppSelector((state) => state.app.isDarkMode);
 
     const { provider, signer } = useWallet();
 
@@ -151,7 +181,7 @@ export default function BuyTokensPage() {
         );
         const nudeWithSigner = nudeContract.connect(signer);
         try {
-            // todo: implement getTokenRate in contract
+            // todo: implement getTokenRate in co   ntract
             const tokenRate = 10;
             const tx = await nudeWithSigner.buyTokens(price, {
                 value: ethers.utils.parseEther(price).div(tokenRate),
@@ -165,27 +195,47 @@ export default function BuyTokensPage() {
 
     return (
         <PageContainer>
-            <MintTitle> $Nude Swap
+            <NudeTitle>
+                {" "}
+                $Nude Swap
                 <MintFormHeaderCandy src={pinkCandy} />
-            </MintTitle>
-            <BuyTokensPageContainer $isDarkMode={isDarkMode}>
+            </NudeTitle>
+            <NudeSwapContainer $isDarkMode={isDarkMode}>
                 <ToggleContainer $isDarkMode={isDarkMode}>
-                    <BuyToken onClick={handleSubmit}>
+                    <BuyTokenButton
+                        key={1}
+                        id={"1"}
+                        onClick={handleBuyClick}
+                        $isBuyActive={isBuyActive}
+    
+                    >
                         Buy Tokens
-                    </BuyToken>
-                    <SellToken onClick={handleSubmit}>
+                    </BuyTokenButton>
+                    <SellTokenButton
+                        key={2}
+                        id={"2"}
+                        onClick={handleSellClick}
+                        $isSellActive={isSellActive}
+                    >
                         Sell Tokens
-                    </SellToken>
+                    </SellTokenButton>
                 </ToggleContainer>
-                <InputDiv>
+                <InputDiv $isSellActive={isSellActive}>
                     <div>
                         <Label>From:</Label>
                         <InputContainer $isDarkMode={isDarkMode}>
-                            <TokenDropdown>
-                                <option value="0">Select Token</option>
-                                <option value="1">Token 1</option>
-                            </TokenDropdown>
-                            <TokenAmoutnInput type="number" value="0.01" max="100000000" />
+                            <InputDivider>
+                                <TokenDropdown>
+                                    <option value="0">Select Token</option>
+                                    <option value="1">Token 1</option>
+                                </TokenDropdown>
+                            </InputDivider>
+                            <TokenAmoutnInput
+                                $isDarkMode={isDarkMode}
+                                type="number"
+                                value="0.01"
+                                max="100000000"
+                            />
                             <MaxButton>Max</MaxButton>
                         </InputContainer>
                     </div>
@@ -193,15 +243,24 @@ export default function BuyTokensPage() {
                     <div>
                         <Label>To:</Label>
                         <InputContainer $isDarkMode={isDarkMode}>
-                            <Label>   <CandyLogo src={candylogo} alt="logo" />$NUDE </Label>
-                            <TokenAmoutnInput type="number" value="0.01" />
-                            <div style={{width: "22%"}}> </div>
+                            <InputDivider>
+                                <Label>
+                                    {" "}
+                                    <CandyLogo src={candylogo} alt="logo" />
+                                    $NUDE{" "}
+                                </Label>
+                            </InputDivider>
+                            <TokenAmoutnInput
+                                $isDarkMode={isDarkMode}
+                                type="number"
+                                value="0.01"
+                            />
+                            <InputDivider> </InputDivider>
                         </InputContainer>
                     </div>
                 </InputDiv>
                 <TransferButton onClick={handleSubmit}>Transfer</TransferButton>
-
-            </BuyTokensPageContainer>
-        </PageContainer >
+            </NudeSwapContainer>
+        </PageContainer>
     );
 }
